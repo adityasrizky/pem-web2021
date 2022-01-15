eb7cc2e7fe534ff5526a4145360f1461b652d48a
Aditya Saiful Rizky <88574985+adityasrizky@users.noreply.github.com>
2022-01-16 05:19:43 +0700

    Add files via upload

---
+++ TugasAkhir/file.js
@@ -0,0 +1,95 @@
+function changeText(text) {
+    var display = document.getElementById('text-display');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function defaultText(text) {
+    var display = document.getElementById('text-display');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function changeText2(text) {
+    var display = document.getElementById('text-display-2');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function defaultText2(text) {
+    var display = document.getElementById('text-display-2');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function changeText3(text) {
+    var display = document.getElementById('text-display-3');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function defaultText3(text) {
+    var display = document.getElementById('text-display-3');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function changeText4(text) {
+    var display = document.getElementById('text-display-4');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function defaultText4(text) {
+    var display = document.getElementById('text-display-4');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function changeText5(text) {
+    var display = document.getElementById('text-display-5');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function defaultText5(text) {
+    var display = document.getElementById('text-display-5');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function changeText6(text) {
+    var display = document.getElementById('text-display-6');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function defaultText6(text) {
+    var display = document.getElementById('text-display-6');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function changeText7(text) {
+    var display = document.getElementById('text-display-7');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function defaultText7(text) {
+    var display = document.getElementById('text-display-7');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function changeText8(text) {
+    var display = document.getElementById('text-display-8');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+function defaultText8(text) {
+    var display = document.getElementById('text-display-8');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
